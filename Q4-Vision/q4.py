6a6061ebe56018da75c99f0360ae56c3864ea5f2
Ziyun Ge <ziyunge1999@gmail.com>
2023-02-20 16:03:42 -0800

    finish q4

--- Q4-Vision/q4.py
+++ Q4-Vision/q4.py
@@ -23,7 +23,7 @@ class sheetDetection:
         sorted_corners = np.array([corners[i] for i in sort_indices][-4:])
         sorted_ids = np.array([ids[i] for i in sort_indices][-4:])
         img_with_markers = cv2.aruco.drawDetectedMarkers(self.img1.copy(), sorted_corners, sorted_ids)
-        cv2.imwrite(f'{self.images_path}/markers_detected.jpg', img_with_markers)
+        # cv2.imwrite(f'{self.images_path}/markers_detected.jpg', img_with_markers)
 
         x, y, w, h = cv2.boundingRect(sorted_corners.reshape(-1, 2))
         self.img1 = self.img1[y:y+h, x:x+w]
@@ -34,13 +34,28 @@ class sheetDetection:
         self.img_file=img_file
         self.img1= cv2.imread(os.path.join(self.captures_path, self.img_file))
         self.detectMarkersAndCrop()
-        cv2.imwrite(os.path.join(self.images_path, "1-img.jpg"), self.img1)
+        # cv2.imwrite(os.path.join(self.images_path, "1-img.jpg"), self.img1)
         self.image_size=self.img1.shape
         self.line_thickness=int(self.image_size[0]/300)
 
     def find_corners(self):
         print("find_corners:")
-        self.mycorners=[(1500,1000),(2500,2000)]
+        # self.mycorners=[(1500,1000),(2500,2000)]
+        blue_lower = (90, 25, 25)
+        blue_upper = (130, 255, 255)
+        hsv = cv2.cvtColor(self.img1, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, blue_lower, blue_upper)
+        # cv2.imwrite(os.path.join(self.images_path, "mask.jpg"), mask)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        largest_contour = max(contours, key=cv2.contourArea)
+        perimeter = cv2.arcLength(largest_contour, True)
+        corners = cv2.approxPolyDP(largest_contour, 0.002 * perimeter, True)
+        contours_img = self.img1.copy()
+        cv2.drawContours(contours_img, largest_contour, -1, (0, 255, 0), 2)
+        # cv2.imwrite(os.path.join(self.images_path, "contours_img.jpg"), contours_img)
+        self.mycorners = [corner[0] for corner in corners]
+        print(self.mycorners)
+
 
     def output_image(self):
         print("output_image:")
